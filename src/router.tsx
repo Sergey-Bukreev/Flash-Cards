998d51df347143a0ecb1b149a6bc0e49854d20f9
BukreevSergey <bukreevwork2@gmail.com>
2024-05-30 16:12:58 +0300

    Adding router and api for project

---
+++ src/router.tsx
@@ -0,0 +1,41 @@
+import {
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom'
+
+import { DecksPage } from '@/pages/decs.page'
+
+const publicRoutes: RouteObject[] = [
+  {
+    element: <div>login</div>,
+    path: '/login',
+  },
+]
+
+const privateRoutes: RouteObject[] = [
+  {
+    element: <DecksPage />,
+    path: '/',
+  },
+]
+
+function PrivateRoutes() {
+  const isAuthenticated = true
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
+}
+
+const router = createBrowserRouter([
+  {
+    children: privateRoutes,
+    element: <PrivateRoutes />,
+  },
+  ...publicRoutes,
+])
+
+export function Router() {
+  return <RouterProvider router={router} />
+}
